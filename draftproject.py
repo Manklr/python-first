9ab99bcc77c5ce261d776b8fada421cdf9e917fa
Zach <Manklr90@yahoo.com>
2022-05-15 02:09:54 -0500

    Committed
    Signed-off-by: Zach <Manklr90@yahoo.com>

---
+++ draftproject.py
@@ -0,0 +1,23 @@
+
+def display_results(weathers,city):
+   print("{}'s temperature: {}°C ".format(city,weathers['main']['temp']))
+   print("Wind speed: {} m/s".format(weathers['wind']['speed']))
+   print("Description: {}".format(weathers['weather'][0]['description']))
+   print("Weather: {}".format(weathers['weather'][0]['main']))
+
+
+def main():
+   
+   city=input('Enter the city:')
+   print()
+   
+   try:
+      query='q='+city;
+      w_data=weather_data(query);
+      display_results(w_data, city)
+      print()
+   except:
+      print('City name not found')
+
+if __name__=='__main__':
+   main()
